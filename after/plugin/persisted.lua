a5f5efd73d85b789b4c2993789f5f0020e0fba3b
MMMaellon <mmmaellon@gmail.com>
2025-09-01 15:48:54 -0700

    Fixed persistence

--- after/plugin/persisted.lua
+++ after/plugin/persisted.lua
@@ -17,6 +17,7 @@ require('persisted').setup({
 
 -- forces it to still save session if we opened it by double-clicking a file
 local persisted = require("persisted")
+local file_opened_from_arg = "";
 vim.api.nvim_create_autocmd("VimEnter", {
   nested = true,
   callback = function()
@@ -25,20 +26,27 @@ vim.api.nvim_create_autocmd("VimEnter", {
       return
     end
 
-    -- Capture the file arguments passed to the nvim command.
-    -- We will check this *after* loading the session.
-    local files_to_open = {}
+    -- -- Capture the file arguments passed to the nvim command.
+    -- -- We will check this *after* loading the session.
+    -- local files_to_open = {}
+    -- if vim.fn.argc() > 0 then
+    --   for i = 0, vim.fn.argc() - 1 do
+    --     local arg = vim.fn.argv(i)
+    --
+    --     vim.notify("ARG " .. i .. ": " .. arg);
+    --
+    --     -- We only care about arguments that are actual, readable files.
+    --     -- This will ignore directories like '.' or 'src/'.
+    --     if vim.fn.filereadable(arg) == 1 then
+    --       vim.notify("Adding to the list of files to open: " .. arg);
+    --       table.insert(files_to_open, arg)
+    --     end
+    --   end
+    -- end
     if vim.fn.argc() > 0 then
       for i = 0, vim.fn.argc() - 1 do
         local arg = vim.fn.argv(i)
-
-        -- vim.notify("ARG " .. i .. ": " .. arg);
-
-        -- We only care about arguments that are actual, readable files.
-        -- This will ignore directories like '.' or 'src/'.
-        if vim.fn.filereadable(arg) == 1 then
-          table.insert(files_to_open, arg)
-        end
+        file_opened_from_arg = vim.fn.getcwd() .. "/" .. vim.fn.fnameescape(arg)
       end
     end
 
@@ -55,27 +63,73 @@ vim.api.nvim_create_autocmd("VimEnter", {
     end
 
     persisted.autoload({ force = forceload })
+    -- persisted.load();
 
-    if #files_to_open > 0 then
-      -- vim.fn.fnameescape is important to handle special characters in file paths.
-      -- Open the first file and load it into the current window.
-      vim.cmd("edit " .. vim.fn.fnameescape(files_to_open[1]))
+    -- if #files_to_open > 0 then
+    --   -- vim.fn.fnameescape is important to handle special characters in file paths.
+    --   -- Open the first file and load it into the current window.
+    --   vim.notify("Opening: " .. vim.fn.fnameescape(files_to_open[1]));
+    --   vim.cmd("vsplit " .. vim.fn.fnameescape(files_to_open[1]))
+    --
+    --   -- If there are more files, add them to the buffer list without opening them.
+    --   for i = 2, #files_to_open do
+    --     vim.notify("Adding to Buffers: " .. files_to_open[i]);
+    --     vim.cmd("badd " .. vim.fn.fnameescape(files_to_open[i]))
+    --   end
+    -- end
+  end,
+})
 
-      -- If there are more files, add them to the buffer list without opening them.
-      for i = 2, #files_to_open do
-        vim.cmd("badd " .. vim.fn.fnameescape(files_to_open[i]))
+local persisted_started = false
+vim.api.nvim_create_autocmd("User", {
+  pattern = "PersistedLoadPost",
+  callback = function(session)
+    persisted_started = true
+    if file_opened_from_arg ~= "" and  vim.fn.filereadable(file_opened_from_arg) == 1 then
+      -- Loop through each window ID.
+      for _, win_id in ipairs(vim.api.nvim_list_wins()) do
+        local buf_id = vim.api.nvim_win_get_buf(win_id)
+        local filename = vim.api.nvim_buf_get_name(buf_id)
+        if filename == file_opened_from_arg then
+          return
+        end
       end
+      vim.cmd("vsplit " .. file_opened_from_arg);
+      file_opened_from_arg = ""
     end
   end,
 })
 
-vim.api.nvim_create_autocmd("User", {
-  pattern = "BufReadPost",
+vim.api.nvim_create_autocmd("WinNew", {
   callback = function(session)
     -- Save the currently loaded session passing in the path to the current session
-    persisted.save({ session = vim.g.persisted_loaded_session })
-    vim.notify("Saving Session");
+    local opened_buf_id = session.buf
+    local opened_filename = vim.api.nvim_buf_get_name(opened_buf_id)
+    if persisted_started and opened_filename ~= "" then
+      persisted.save({ session = vim.g.persisted_loaded_session })
+    end
+    -- vim.notify("Saving Session");
     -- Delete all of the open buffers
     -- vim.api.nvim_input("<ESC>:%bd!<CR>")
+  end,
+})
+vim.api.nvim_create_autocmd("WinClosed", {
+  callback = function(session)
+    local open_windows = vim.api.nvim_list_wins();
+    local window_count = 0;
+    local closed_buf_id = session.buf
+    local closed_filename = vim.api.nvim_buf_get_name(closed_buf_id)
+    vim.notify("CLOSED: " .. closed_filename)
+    for _, win_id in ipairs(vim.api.nvim_list_wins()) do
+      local buf_id = vim.api.nvim_win_get_buf(win_id)
+      local filename = vim.api.nvim_buf_get_name(buf_id)
+      if filename ~= "" and filename ~= closed_filename then
+        window_count = window_count + 1;
+      end
+    end
+    vim.notify("window count " .. window_count)
+    if window_count > 0 then
+      persisted.save({ session = vim.g.persisted_loaded_session })
+    end
   end,
 })
--- lua/plugins/_misc.lua
+++ lua/plugins/_misc.lua
@@ -119,6 +119,14 @@ return {
 		"olimorris/persisted.nvim",
 		lazy = false,
 		event = "BufReadPre",
+		config = function(_, opts)
+			local persisted = require("persisted")
+			persisted.branch = function()
+			  local branch = vim.fn.systemlist("git branch --show-current")[1]
+			  return vim.v.shell_error == 0 and branch or nil
+			end
+			persisted.setup(opts)
+		end,
 	},
 	--
 	-- --allows closing hidden buffers
